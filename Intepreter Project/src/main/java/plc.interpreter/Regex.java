c7f247ab9e8612f39e362f50d8862c93b23128cb
erik-rosa <56844083+erik-rosa@users.noreply.github.com>
2020-09-15 16:29:33 -0400

    Updated Regex

--- Intepreter Project/src/main/java/plc.interpreter/Regex.java
+++ Intepreter Project/src/main/java/plc.interpreter/Regex.java
@@ -13,8 +13,8 @@ public class Regex {
             FILE_NAMES = Pattern.compile("([A-Za-z]+)([.]{1}[A-Za-z])*[@]{1}([A-Za-z]+[.]{1}[A-Za-z]+)*[.]{1}(([j][a][v][a]){1}|([c][l][a][s][s]){1})"),
             EVEN_STRINGS = Pattern.compile("([A-Za-z]{10})|([A-Za-z]{12})|([A-Za-z]{14})|([A-Za-z]{16})|([A-Za-z]{18})|([A-Za-z]{20})"),
             INTEGER_LIST = Pattern.compile("\\[(\\d+)(,[ ]{0,1}\\d+)*\\]"),
-            IDENTIFIER = Pattern.compile(""),
-            NUMBER = Pattern.compile(""),
-            STRING = Pattern.compile("");
+            IDENTIFIER = Pattern.compile("(^([_a-zA-Z0-9\\+\\-\\:\\!\\?\\<\\>\\=\\.][^0-9.]))([a-zA-Z0-9\\+\\-\\:\\!\\?\\<\\>\\=\\.])*"),
+            NUMBER = Pattern.compile("^([0-9]+|[\\+\\-][0-9])(\\.[0-9])?[0-9]*$"),
+            STRING = Pattern.compile("^\\\"([^\\\\]|(\\\\b)*|(\\\\n)*|(\\\\r)*|(\\\\t)*|(\\\\\\')*|(\\\\\\\")*|(\\\\\\\\)*)*\\\"$");
 
 }
